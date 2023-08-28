5f6297fdf74f03413f7330d3cc2198d8a7617205
alumno <alumno@Instituto.local>
2023-08-28 11:00:48 -0300

    pA TI
    dsadas

--- java.js
+++ java.js
@@ -1,22 +1,22 @@
- /*let numeroNueve = 9
- let numeroOcho = 8
+/*let numeroNueve = 9
+let numeroOcho = 8
 console.log (numeroNueve)
 console.log (numeroOcho)
 let edad = (2023 - 2009) 
 alert ('mi edad es ' + edad ) 
- let nombre = prompt ( 'cual es tu nombre ' )
- alert ('hola bienvenid@ ' + nombre)
- let edadVisitante =  prompt 
- let nombreAlumno
- let apellidoAlumno
+let nombre = prompt ( 'cual es tu nombre ' )
+alert ('hola bienvenid@ ' + nombre)
+let edadVisitante =  prompt 
+let nombreAlumno
+let apellidoAlumno
 
 nombreAlumno =  prompt ('cual es tu Nombre')
 apellidoAlumno  =  prompt ('y tu apellido')
 alert ('bienvenid@ ' + nombreAlumno +' ' + apellidoAlumno)
 console.log ('hola ' + nombreAlumno + apellidoAlumno)
- let diez = (10)
- let tres = (3)
- resultadoMultiplicacion = ( diez * tres)
+let diez = (10)
+let tres = (3)
+resultadoMultiplicacion = ( diez * tres)
 alert ('el resultado de la multiplicacion es ' + resultadoMultiplicacion)
 
 function calcularPromedio (a,b,c)
@@ -27,41 +27,55 @@ return promedio
 }
 alert (calcularPromedio(7,8,9))
 calcularPromedio = prompt ('ingrese 3 numeros ')
- alert (calcularPromedio)
- /* hacer una funcion que calcula al cubo de  un numero*/ 
- function calcularAlCubo() {
-     let numeroElevado = prompt ('ingrse un numero ')
-      return numeroElevado * numeroElevado * numeroElevado
- }
+alert (calcularPromedio)
+/* hacer una funcion que calcula al cubo de  un numero*/
+/*
+function calcularAlCubo() {
+    let numeroElevado = prompt ('ingrse un numero ')
+     return numeroElevado * numeroElevado * numeroElevado
+}
 
- alert (calcularAlCubo())
+alert (calcularAlCubo())
 
- function multiplicar () {
+function multiplicar () {
 let numeroAmultiplicar = prompt ('ingrese dos numeros separados por comas para multiplicar')
 return numeroAmultiplicar * numeroAmultiplicar
- }
+}
 alert (multiplicar ())
- function dividir () {
-    let numeroAdividir = prompt ('ingrese dos numeros separados por comas para dividir')
-    return numeroAdividir / numeroAdividir
-     }
+function dividir () {
+   let numeroAdividir = prompt ('ingrese dos numeros separados por comas para dividir')
+   return numeroAdividir / numeroAdividir
+    }
 
-     function sumar () {
-        let numeroAsumar = prompt ('ingrese dos numeros separados por comas para sumar')
-        return numeroAsumar + numeroAsumar
-         }
+    function sumar () {
+       let numeroAsumar = prompt ('ingrese dos numeros separados por comas para sumar')
+       return numeroAsumar + numeroAsumar
+        }
 
-         function restar () {
-            let numeroArestar = prompt ('ingrese dos numeros separados por comas restar')
-            return numeroArestar - numeroArestar
-             }
-
-             /* x^2+5*/
-            function operacion(a, b ) {
-let operation = a^2+5
+        function restar () {
+           let numeroArestar = prompt ('ingrese dos numeros separados por comas restar')
+           return numeroArestar - numeroArestar
             }
-           console.log (operacion  (4))
-             
+
+            /* x^2+5*/
+function operacion(a) {
+   let operation = a * a + 5
+   return operation
+}
+console.log(operacion(5))
+
+function  saludo () {
+   let nombresao = prompt("cual es tu nombre ")
+   let apellidino = prompt("cual es tu apellido ")
+   return ("bievenid@ " + nombresao  + + apellidino)
+}
+
+alert (saludo())
+function saludaso (a, b){
+  let saludasao = "bienvenid@ " + a + b 
+   return saludasao 
+}
+alert (saludaso("mixi ", "sidain"))
 
 
 

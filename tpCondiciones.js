0bd4c429dad2b5535b3770cec4b9d3f1c6773889
alumno <alumno@Instituto.local>
2023-10-27 09:28:50 -0300

    hasta la 2 y sabiendo resolver la 3
    jiji

---
+++ tpCondiciones.js
@@ -0,0 +1,15 @@
+function nombresao(x) {
+    if ((x = 'nahuel') || (x = 'santino')){
+        return alert('hola' + x + 'como estas')
+} else {
+    alert('hola')
+}}
+function edaddeshion (edad) {
+    edad  = Number(prompt('ingresa tu edad '))
+    if (edad >= 18) {
+    alert('sos mayor ')
+    return  edad
+}else {
+    alert(' sos menor ')
+}
+}

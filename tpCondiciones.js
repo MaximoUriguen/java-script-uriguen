3bef96f6faac7fd15e2dc69ea44769004653f92c
MaximoUriguen <136344491+MaximoUriguen@users.noreply.github.com>
2023-10-29 18:53:10 -0300

    hasta la 6

--- tpCondiciones.js
+++ tpCondiciones.js
@@ -13,3 +13,52 @@ function edaddeshion (edad) {
     alert(' sos menor ')
 }
 }
+ let  edadCalificativa  = (Number)(prompt ( 'ingresa tu edad ') )
+ function calificacionDeEdad (edadCalificativa){
+    if (edadCalificativa <= 11) 
+    alert ('sos un niño ')
+ } else if 
+    (edadCalificativa <= 18 )
+(alert('sos un adolecente'))
+else if (edadCalificativa <= 26 )
+alert('sos un joven')
+else if 
+    (edadCalificativa <= 59 )
+(alert('sos un adolecente'))
+else alert ('eres un anciano')
+
+let diaDeLaSemana =  (prompt ('que dia es hoy'))
+    function diaLaborable (diaDeLaSemana) {
+        if (diaDeLaSemana = 'martes', 'miercoles', 'lunes', 'jueves' , 'viernes')
+        alert ('es un dia laborable')
+    } else {
+        alert ('aprovecha a descansar es fin de semana ')
+    }
+    let Contraseña =  (prompt ('¿cual es la contraseña?'))
+    function password (contraseña) {
+        if (contraseña = 'secreto')
+        alert ('acceso consedido')
+    } else {
+        alert ('acceso denegado')
+    }
+
+    function Multiplo5 (num) {
+        return num % 5 === 0;
+    }
+    let pelota = 100
+    let edadDescuento = Number(prompt('ingresa tu edad '))
+    function descuento {
+        if (edadDescuento >= 65)
+        alert (' el precio es 90')
+
+    }else ( alert ('el precio son 100'))
+    
+
+
+
+
+
+
+
+ 
+ 

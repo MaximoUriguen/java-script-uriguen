51c52a4e133dc4c39f88dcbf3d2ffb85475f2a3a
alumno <alumno@Instituto.local>
2023-11-10 09:29:56 -0300

    no anda

--- dom.js
+++ dom.js
@@ -1,5 +1,24 @@
 let imagen = document.querySelector ('h1')
-titulo.innertext = 'hola como estas'
+
+
 
 let imagen = document.querySelector ('.imagen')
 imagen.src = 'https://ideogram.ai/g/OMMmEYmGSHy-OyCh7thcpQ/0'
+let titulo = document.querySelector ('h1')
+
+let botonRojo = document.querySelector ('#rojo')
+let botonverde = document.querySelector ('#verde')
+let botonazul = document.querySelector ('#azul')
+let botonnegro = document.querySelector ('#negro')
+botonRojo.onclick = function (){
+    titulo.style.color = 'red'
+}
+botonverde.onclick = function (){
+    titulo.style.color = 'green'
+}
+botonazul.onclick = function (){
+    titulo.style.color = 'blue'
+}
+botonanegro.onclick = function (){
+    titulo.style.color = 'black'
+}
